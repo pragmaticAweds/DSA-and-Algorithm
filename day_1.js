a04e360d88ab1cadf376f0a040261c51dcecacfc
pragmaticAweds <jimohafeez738@gmail.com>
2024-07-22 23:59:53 +0100

    feat: settin up dsa

---
+++ day_1.js
@@ -0,0 +1,3 @@
+// You are given an array of Integers in which each subsequent value is not less than the previous value. Write a function that takes this array as an input and returns a new array with the squares of each number sorted in ascending order.
+
+// An array is monotonic if it is either monotone increasing or monotone decreasing. An array is monotone increasing if all its elements from left to right are non-decreasing. An array is monotone decreasing if all  its elements from left to right are non-increasing. Given an integer array return true if the given array is monotonic, or false otherwise.
